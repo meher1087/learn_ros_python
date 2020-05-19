d8d4956eb13f441f31920217f4c553a4bb05924e
meher1087 <meher1087@gmail.com>
2020-05-19 11:38:59 +0530

    arm_ws work space added where dexter arm is totally controlled using moveit setup assitant with position controllers. Python node is written to get arm position and plot it in matplotlib in runtime. TODO - have to execute python command node to control arm and get feedback and plot.

--- ws_moveit/build/panda_moveit_config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mady/MEGA/mady/courses/learn_ros_python/ws_moveit/src/panda_moveit_config
-# Build directory: /home/mady/MEGA/mady/courses/learn_ros_python/ws_moveit/build/panda_moveit_config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ws_moveit/build/ros-moveit-arm/my_arm_xacro/CMakeFiles/test_robot_arm_moveit_ikfast_plugin.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_robot_arm_moveit_ikfast_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
